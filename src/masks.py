69c58f504d504673b67465b3497e7b18a2255baa
Андрей Лагунов <tobeplayed@mail.ru>
2024-07-03 22:47:46 +0800

    Первичное добавление файлов проекта

---
+++ src/masks.py
@@ -0,0 +1,36 @@
+LENGTH_ERROR_MESSAGE = "В функцию переданы данные, длина которых отлична от 16 символов/цифр"
+TYPE_ERROR_MESSAGE = "В функцию был передан тип, отличный от str и int"
+
+
+def get_mask_card_number(card_number: str | int) -> str:
+    """
+    Функция принимает номер карты (строка или целое) вида 1111222233334444,
+    возвращает строку вида 1111 22** **** 4444
+    """
+    if type(card_number) not in (str, int):
+        raise TypeError(TYPE_ERROR_MESSAGE)
+
+    if type(card_number) is not str:
+        card_number = str(card_number)
+
+    if len(card_number) != 16:
+        raise ValueError(LENGTH_ERROR_MESSAGE)
+
+    return f"{card_number[0:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(card_number: str | int) -> str:
+    """
+    Функция принимает номер карты (строка или целое) вида 1111222233334444,
+    возвращает строку вида **4444
+    """
+    if type(card_number) not in (str, int):
+        raise TypeError(TYPE_ERROR_MESSAGE)
+
+    if type(card_number) is not str:
+        card_number = str(card_number)
+
+    if len(card_number) != 16:
+        raise ValueError(LENGTH_ERROR_MESSAGE)
+
+    return f"**{card_number[-4:]}"
