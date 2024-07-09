e0b54c94bf486bf8cde0df7fd63a72676a9e5fd2
Андрей Лагунов <tobeplayed@mail.ru>
2024-07-09 21:51:58 +0800

    Добавлена функция filter_by_state в модуль processing

---
+++ src/processing.py
@@ -0,0 +1,49 @@
+from widget import get_date
+
+
+def filter_by_state(initial_lst: list[dict], state="EXECUTED") -> list[dict]:
+    """
+    Принимает список словарей
+    Возвращает список словарей, фильтрованный по state
+    """
+    return [dict_ for dict_ in initial_lst if dict_["state"] == state]
+
+
+# list_to_filtering = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# print(filter_by_state(list_to_filtering))
+# print(filter_by_state(list_to_filtering, state="CANCELED"))
+
+
+def sort_by_date(initial_lst: list[dict], is_sorting_down=True) -> list[dict]:
+    """
+    Принимает список словарей и необязательный параметр, задающий порядок сортировки.
+    Возвращать новый список, отсортированный по дате (по умолчанию — убывание).
+    """
+    # Промежуточный список, в котором даты приведены к значению float (дробное кол-во лет)
+    # Пример    11.03.2024 -> 2024 + 3/12 + 11/31 -> float
+    temp_list = []
+    for dict_ in initial_lst:
+        date_str = get_date(dict_["date"])
+        years = int(date_str[-4:])
+        months = int(date_str[3:5])
+        days = int(date_str[:2])
+        date_float = years + months / 12 + days / 31
+        print(dict_, date_str, date_float)
+
+
+    # sorted_lst = sorted(initial_lst, key=lambda dict_: get_date(dict_["date"]))
+    # return sorted_lst
+
+
+list_to_sort = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+print(sort_by_date(list_to_sort))
+print(sort_by_date(list_to_sort, is_sorting_down=False))
