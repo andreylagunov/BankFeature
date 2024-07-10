5e02deca8ae75c5a17f1e34151df40c255fd743d
Андрей Лагунов <tobeplayed@mail.ru>
2024-07-10 22:25:48 +0800

    В модуль processing добавлены функции filter_by_state, sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
-from widget import get_date
+from src.widget import get_date
 
 
-def filter_by_state(initial_lst: list[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(initial_lst: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает список словарей
     Возвращает список словарей, фильтрованный по state
@@ -13,37 +13,34 @@ def filter_by_state(initial_lst: list[dict], state="EXECUTED") -> list[dict]:
 #                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #                         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 #                         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-#
 # print(filter_by_state(list_to_filtering))
 # print(filter_by_state(list_to_filtering, state="CANCELED"))
 
 
-def sort_by_date(initial_lst: list[dict], is_sorting_down=True) -> list[dict]:
+def sort_by_date(initial_lst: list[dict], is_sorting_down: bool = True) -> list[dict]:
     """
     Принимает список словарей и необязательный параметр, задающий порядок сортировки.
-    Возвращать новый список, отсортированный по дате (по умолчанию — убывание).
+    Возвращает новый список, отсортированный по дате (по умолчанию — убывание).
     """
-    # Промежуточный список, в котором даты приведены к значению float (дробное кол-во лет)
-    # Пример    11.03.2024 -> 2024 + 3/12 + 11/31 -> float
-    temp_list = []
-    for dict_ in initial_lst:
-        date_str = get_date(dict_["date"])
+
+    def get_float_date(string: str) -> float:
+        """
+        Используется для промежуточного представления даты.
+        Принимает дату вида     2024-03-11T02:26:18.671407
+        Возвращает float (лет)  2024.2533...
+        """
+        date_str = get_date(string)
         years = int(date_str[-4:])
         months = int(date_str[3:5])
         days = int(date_str[:2])
-        date_float = years + months / 12 + days / 31
-        print(dict_, date_str, date_float)
+        return years + months / 12 + days / 365
+
+    return sorted(initial_lst, key=lambda dict_: get_float_date(dict_["date"]), reverse=is_sorting_down)
 
 
-    # sorted_lst = sorted(initial_lst, key=lambda dict_: get_date(dict_["date"]))
-    # return sorted_lst
-
-
-list_to_sort = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
-print(sort_by_date(list_to_sort))
-print(sort_by_date(list_to_sort, is_sorting_down=False))
+# list_to_sort = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                   {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+# print(sort_by_date(list_to_sort))
+# print(sort_by_date(list_to_sort, is_sorting_down=False))
