5997a304659937f5236e7fa679cd470f5de59d4d
Андрей Лагунов <tobeplayed@mail.ru>
2024-07-10 22:30:06 +0800

    В модуле widget закомментированы проверки (assert)

--- src/widget.py
+++ src/widget.py
@@ -54,15 +54,13 @@ def get_date(date_str: str) -> str:
         raise ValueError("Некорректный формат даты/времени")
 
 
-assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
-assert mask_account_card("Maestro 1596 8378 6870 5199") == "Maestro 1596 83** **** 5199"
-assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
-assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
-assert mask_account_card("Счет 35383033474447895560") == "Счет **5560"
-assert mask_account_card("Visa Classic 6831982476737658") == "Visa Classic 6831 98** **** 7658"
-assert mask_account_card("Visa Platinum 8990922113665229") == "Visa Platinum 8990 92** **** 5229"
-assert mask_account_card("Visa Gold 5999414228426353") == "Visa Gold 5999 41** **** 6353"
-assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
-
-
-assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
+# assert mask_account_card("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
+# assert mask_account_card("Maestro 1596 8378 6870 5199") == "Maestro 1596 83** **** 5199"
+# assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
+# assert mask_account_card("MasterCard 7158300734726758") == "MasterCard 7158 30** **** 6758"
+# assert mask_account_card("Счет 35383033474447895560") == "Счет **5560"
+# assert mask_account_card("Visa Classic 6831982476737658") == "Visa Classic 6831 98** **** 7658"
+# assert mask_account_card("Visa Platinum 8990922113665229") == "Visa Platinum 8990 92** **** 5229"
+# assert mask_account_card("Visa Gold 5999414228426353") == "Visa Gold 5999 41** **** 6353"
+# assert mask_account_card("Счет 73654108430135874305") == "Счет **4305"
+# assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
